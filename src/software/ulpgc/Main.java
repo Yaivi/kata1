de8b4ffdbb976d3aebdb541490001ae125933f28
Javier Sánchez Godoy <81640937+Yaivi@users.noreply.github.com>
2024-12-04 17:39:52 +0000

    Main creado, prueba de Person completa

---
+++ src/software/ulpgc/Main.java
@@ -0,0 +1,9 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Person("Javier", LocalDate.of(2003,11,1)));
+    }
+}
